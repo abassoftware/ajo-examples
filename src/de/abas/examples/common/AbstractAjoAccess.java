6475278bb0641d9fcb9e60e497711883e96a87ac
Jasmin Scheffel <jasc@abas.de>
2014-12-02 16:26:48 +0100

    AbstractAjoAccess.java updated

--- src/de/abas/examples/common/AbstractAjoAccess.java
+++ src/de/abas/examples/common/AbstractAjoAccess.java
@@ -11,19 +11,49 @@ import de.abas.erp.db.DbMessage;
 import de.abas.erp.db.MessageListener;
 import de.abas.erp.db.util.ContextHelper;
 
+/**
+ * Utility method for getting the client and/or server context.
+ *
+ * @author abas Software AG
+ *
+ */
 public abstract class AbstractAjoAccess implements ContextRunnable {
-	
-	// define EDP connection properties
-	private String hostname = "schulung";
-	private String mandant = "i7erp4";
-	private String password = "sy";
-	private int port = 6550;
 
-	private FileWriter fileWriterLogging;
-	
-	// Enumeration: Status
+	/**
+	 * Context mode enumeration.
+	 *
+	 * @author abas Software AG
+	 *
+	 */
+	public enum ContextMode {
+		UNDEFINED {
+			@Override
+			public String toString() {
+				return "";
+			}
+		},
+		SERVER_MODE {
+			@Override
+			public String toString() {
+				return "server-mode";
+			}
+		},
+		CLIENT_MODE {
+			@Override
+			public String toString() {
+				return "client-mode";
+			}
+		}
+	}
+
 	public enum Status {
-		UNDEFINED{
+		/**
+		 * Status enumeration.
+		 *
+		 * @author abas Software AG
+		 *
+		 */
+		UNDEFINED {
 			@Override
 			public String toString() {
 				return "";
@@ -45,179 +75,213 @@ public abstract class AbstractAjoAccess implements ContextRunnable {
 			}
 		}
 	}
-	
-	// Enumeration: ContextMode
-	public enum ContextMode {
-		UNDEFINED {
-			@Override
-			public String toString() {
-				return "";
-			}
-		},
-		SERVER_MODE {
-			@Override
-			public String toString() {
-				return "server-mode";
-			}
-		},
-		CLIENT_MODE {
-			@Override
-			public String toString() {
-				return "client-mode";
-			}
-		}
-	}
-	
-	// Initialize DbContext 
+
+	// define EDP connection properties
+	private String hostname = "schulung";
+	private String mandant = "i7erp4";
+
+	private String password = "sy";
+
+	private int port = 6550;
+
+	private FileWriter fileWriterLogging;
+
+	// Initialize DbContext
 	private DbContext dbContext = null;
 	private ContextMode mode = ContextMode.UNDEFINED;
-	
-	
-	// Abstrakte run-Methode definieren. Diese muss in allen abgeleiteten Klassen eingebaut 
-	// und vervollständigt werden.
-	public abstract void run(String[] args);
-	
-	
-	// server access: get server context. Initialize mode
-	public int runFop(FOPSessionContext fopSessionContext, String[] args) throws FOPException {
-		this.dbContext = fopSessionContext.getDbContext();
-		this.mode = ContextMode.SERVER_MODE;
-		// run-methode aufrufen. Argumente übergeben
-		run(args);
-		return 0;
-	}
-	
-	
-	// if content of dbContext == null. Initialize mode
-	public final void runClientProgram(String[] args) {
-		run(args);
-		// Protokollierung abschalten
-		disableLogging();
-		// Datenbankkontext schließen
-		getDbContext().close();
-	}
-	
-	
-	// disable EDP-logging
-	private void disableLogging() {
-		if (null != fileWriterLogging) {
-			try {
-				fileWriterLogging.close();
-			} catch (IOException e) {
-				getDbContext().out().println(e.getMessage());
-			} finally {
-				fileWriterLogging = null;
-			}
-		}
-		getDbContext().setLogger(null);
-	}
-	
-	// enable EDP-logging. Creates log file name, using class description
-	public final void enableLogging() {
-		enableLogging(getClass().getSimpleName() + ".log");
-	}
 
-	// enable EDP-logging. creates log file name, using parameter filename
-	public void enableLogging(String fileName) {
-		try {
-			fileWriterLogging = new FileWriter(fileName);
-			getDbContext().setLogger(fileWriterLogging);
-		} catch (IOException e) {
-			getDbContext().out().println(e.getMessage());
-		}
-	}
-
-	// add default message listener.
-	// gets all text, status and error messages
+	/**
+	 * Adds a default message listener. Gets all text, status and error messages.
+	 */
 	public void addDefaultMessageListener() {
 		getDbContext().addMessageListener(new MessageListener() {
 			// Displays all text, status and error messages
+			@Override
 			public void receiveMessage(DbMessage message) {
-				getDbContext().out().println("|"+ message +"|");
+				getDbContext().out().println("|" + message + "|");
 			}
 
 		});
 	}
 
-	
-	// get DbContext
-	// creates a Client-Context if dbContext == null
-	public DbContext getDbContext() {
-		if(this.dbContext == null){
-			this.dbContext = ContextHelper.createClientContext(hostname, port, mandant, password, this.getClass().getSimpleName());
-			mode = ContextMode.CLIENT_MODE;
-		}
-		return this.dbContext;
-	}
-	
-	// get dbContext mode
-	public String getMode() {
-		return this.mode.toString();
+	/**
+	 * Enables EDP logging. Creates a log file (name is class name).
+	 */
+	public final void enableLogging() {
+		enableLogging(getClass().getSimpleName() + ".log");
 	}
 
-	// Connection parameter
-	// get hostname
+	/**
+	 * Enables EDP logging. Creates a log file (name as defined in parameter).
+	 *
+	 * @param fileName Name of log file.
+	 */
+	public void enableLogging(String fileName) {
+		try {
+			fileWriterLogging = new FileWriter(fileName);
+			getDbContext().setLogger(fileWriterLogging);
+		}
+		catch (IOException e) {
+			getDbContext().out().println(e.getMessage());
+		}
+	}
+
+	/**
+	 * Gets the database context. Create a client context if dbContext == null
+	 *
+	 * @return The database context.
+	 */
+	public DbContext getDbContext() {
+		if (dbContext == null) {
+			dbContext = ContextHelper.createClientContext(hostname, port, mandant, password, this.getClass().getSimpleName());
+			mode = ContextMode.CLIENT_MODE;
+		}
+		return dbContext;
+	}
+
+	/**
+	 * Returns the host name.
+	 *
+	 * @return The host name.
+	 */
 	public String getHostname() {
 		return hostname;
 	}
 
-	// if client context is running, set hostname 
-	// close dbContext and define dbContont = null
-	public void setHostname(String hostname) {
-		if (isClientContextRunning()) {
-			this.hostname = hostname;
-			dbContext.close();
-			this.dbContext = null;
-		}
-	}
-
-	// get mandant
+	/**
+	 * Returns the client.
+	 *
+	 * @return The client.
+	 */
 	public String getMandant() {
 		return mandant;
 	}
 
-	// if client context is running, set mandant
-	// close dbContext and define dbContont = null
-	public void setMandant(String mandant) {
-		if (isClientContextRunning()) {
-			this.mandant = mandant;
-			dbContext.close();
-			this.dbContext = null;
-		}
+	/**
+	 * Returns the context mode.
+	 *
+	 * @return The context mode.
+	 */
+	public String getMode() {
+		return mode.toString();
 	}
 
-	// get port
+	/**
+	 * Returns the port.
+	 *
+	 * @return The port.
+	 */
 	public int getPort() {
 		return port;
 	}
 
-	// if client context is running, set port
-	// close dbContext and define dbContont = null
-	public void setPort(int port) {
+	/**
+	 * Abstract run method to implement in all derived classes.
+	 *
+	 * @param args
+	 */
+	public abstract void run(String[] args);
+
+	/**
+	 * Runs program in client mode. Creates client context if dbContext==null
+	 *
+	 * @param args
+	 */
+	public final void runClientProgram(String[] args) {
+		run(args);
+		disableLogging();
+		getDbContext().close();
+	}
+
+	// server access: get server context. Initialize mode
+	@Override
+	public int runFop(FOPSessionContext fopSessionContext, String[] args) throws FOPException {
+		dbContext = fopSessionContext.getDbContext();
+		mode = ContextMode.SERVER_MODE;
+		run(args);
+		return 0;
+	}
+
+	/**
+	 * Sets the host name if client context is running.
+	 *
+	 * @param hostname The host name.
+	 */
+	public void setHostname(String hostname) {
 		if (isClientContextRunning()) {
-			this.port = port;
+			this.hostname = hostname;
 			dbContext.close();
-			this.dbContext = null;
+			dbContext = null;
 		}
 	}
 
-	// if client context is running, set passwort
-	// close dbContext and define dbContont = null
+	/**
+	 * Sets the client if client context is running.
+	 *
+	 * @param mandant The client.
+	 */
+	public void setMandant(String mandant) {
+		if (isClientContextRunning()) {
+			this.mandant = mandant;
+			dbContext.close();
+			dbContext = null;
+		}
+	}
+
+	/**
+	 * Sets the password if client context is running.
+	 *
+	 * @param password The password.
+	 */
 	public void setPassword(String password) {
 		if (isClientContextRunning()) {
 			this.password = password;
 			dbContext.close();
-			this.dbContext = null;
+			dbContext = null;
 		}
 	}
 
-	// is client context running?
-	// true -> client is running
-	// false -> client context is not running
+	/**
+	 * Sets the port if client context is running.
+	 *
+	 * @param port The port.
+	 */
+	public void setPort(int port) {
+		if (isClientContextRunning()) {
+			this.port = port;
+			dbContext.close();
+			dbContext = null;
+		}
+	}
+
+	/**
+	 * Disables EDP logging
+	 */
+	private void disableLogging() {
+		if (null != fileWriterLogging) {
+			try {
+				fileWriterLogging.close();
+			}
+			catch (IOException e) {
+				getDbContext().out().println(e.getMessage());
+			}
+			finally {
+				fileWriterLogging = null;
+			}
+		}
+		getDbContext().setLogger(null);
+	}
+
+	/**
+	 * Checks whether the client context is running.
+	 *
+	 * @return true -> client context is running, false -> client context is not running
+	 */
 	private boolean isClientContextRunning() {
 		if (mode.equals(ContextMode.CLIENT_MODE)) {
 			return true;
-		} else {
+		}
+		else {
 			dbContext.out().println("No Client-Mode running -> parameter may not be changed");
 			return false;
 		}
