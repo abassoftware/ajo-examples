5029a0aad109856071c481ee034bd815242ee786
Jasmin Scheffel <jasc@abas.de>
2014-12-15 08:38:35 +0100

    example for language independency updated

--- src/de/abas/examples/languageindependency/LanguageIndependentTexts.java
+++ src/de/abas/examples/languageindependency/LanguageIndependentTexts.java
@@ -6,11 +6,13 @@ import java.util.ResourceBundle;
 import de.abas.eks.jfop.FOPException;
 import de.abas.eks.jfop.remote.ContextRunnable;
 import de.abas.eks.jfop.remote.EKS;
+import de.abas.eks.jfop.remote.FO;
 import de.abas.eks.jfop.remote.FOPSessionContext;
 import de.abas.erp.api.gui.TextBox;
 
 /**
- * This class shows how to use language independent texts. The translation of each text is stored in a Property file with the locale as extension in the file.
+ * This class shows how to use language independent texts. The translation of each
+ * text is stored in a Property file with the locale as extension in the file.
  *
  * Property files can be translated in abas ERP using the infosystem TRANS.
  *
@@ -21,35 +23,44 @@ public class LanguageIndependentTexts implements ContextRunnable {
 
 	@Override
 	public int runFop(FOPSessionContext ctx, String[] args) throws FOPException {
-		new TextBox(ctx.getDbContext(), getTextInOperatingLanguage("LanguageIndependentTexts.1", null), getTextInOperatingLanguage("LanguageIndependentTexts.2", null)).show();
-		new TextBox(ctx.getDbContext(), getTextInOperatingLanguage("LanguageIndependentTexts.1", null), getTextInOperatingLanguage("LanguageIndependentTexts.3",
-				new String[] { "parameter" })).show();
+		// TextBox with simple text
+		new TextBox(ctx.getDbContext(),
+				getTextInOperatingLanguage("LanguageIndependentTexts.1"),
+				getTextInOperatingLanguage("LanguageIndependentTexts.2")).show();
+
+		// TextBox containing variable text
+		String param =
+				FO.lesen(new String[] { getTextInOperatingLanguage("LanguageIndependentTexts.4") });
+		new TextBox(ctx.getDbContext(),
+				getTextInOperatingLanguage("LanguageIndependentTexts.1"),
+				getTextInOperatingLanguage("LanguageIndependentTexts.3", param))
+				.show();
+
 		return 0;
 	}
 
 	/**
-	 * Gets text specified by key from LanguageIndependentTexts_lang.properties in current operating language
+	 * Gets text specified by key from LanguageIndependentTexts_lang.properties in
+	 * current operating language
 	 *
 	 * @param key The key of the text in ControlFOPCopySystem.properties.
-	 * @param params The array containing the Strings to change for the placeholder within the bundle.
+	 * @param params Strings containing parameters needed for bundle text.
 	 * @return Returns text in current operating language.
 	 */
-	protected String getTextInOperatingLanguage(String key, String[] params) {
+	protected String getTextInOperatingLanguage(String key, Object... params) {
 		// gets current operating language
-		Locale operatingLangLocale = EKS.getFOPSessionContext().getOperatingLangLocale();
+		Locale operatingLangLocale =
+				EKS.getFOPSessionContext().getOperatingLangLocale();
 		// gets text specified by key from ControlFOPCopySystem.properties in
 		// previously specified operating language
-		String bundle = ResourceBundle.getBundle(LanguageIndependentTexts.class.getName(), operatingLangLocale, LanguageIndependentTexts.class.getClassLoader()).getString(key);
-		// fills replacement characters with the according parameters from
+		String bundle =
+				ResourceBundle.getBundle(LanguageIndependentTexts.class.getName(),
+						operatingLangLocale,
+						LanguageIndependentTexts.class.getClassLoader()).getString(
+								key);
+		// fills replacement characters (%s) with the according parameters from
 		// params
-		if (!(params == null)) {
-			if (!(params.length == 0)) {
-				for (String param : params) {
-					bundle = bundle.replaceFirst("\\{[0-9]+\\}", param);
-				}
-			}
-		}
-		return bundle;
+		return String.format(bundle, params);
 	}
 
 }
