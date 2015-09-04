d99e52518bc3d092f53d29e77d745f5bf65779e6
jasc <jasc@abas.de>
2015-09-04 11:05:22 +0200

    InputBox example

---
+++ src/de/abas/examples/inputbox/InputBoxExample.java
@@ -0,0 +1,26 @@
+package de.abas.examples.inputbox;
+
+import de.abas.erp.api.gui.InputBox;
+import de.abas.erp.db.DbContext;
+import de.abas.examples.common.AbstractAjoAccess;
+
+public class InputBoxExample extends AbstractAjoAccess {
+	
+	DbContext ctx;
+
+	@Override
+	public int run(String[] args) {
+		ctx = getDbContext();
+		
+		InputBox inputBox = new InputBox(ctx, "Normal Text goes here:");
+		String input = inputBox.read();
+		ctx.out().println("You entered: " + input);
+		
+		InputBox viewProtectedInputBox = new InputBox(ctx, "View Protected Text goes here:");
+		String viewProtectedInput = viewProtectedInputBox.read(true);
+		ctx.out().println("It was view protected, but I can still read it. You entered: " + viewProtectedInput);
+		
+		return 0;
+	}
+	
+}
